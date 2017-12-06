8e9080f28a2129f06b09b39a324296f87f184820
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2017-12-06 11:47:16 -0500

    Bumps version to 1.1.0

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register :queries_issues_count do
   name 'Queries Issues Count'
   author 'eXolnet'
   description 'This plugin allow user to add issues count for theirs custom reports.'
-  version '1.0.0'
+  version '1.1.0'
   url 'https://redmine.exolnet.com/projects/redmine-queries-issues-count'
   author_url 'http://www.exolnet.com'
 end
