ea6d312ad5e5c0eda0123a27f3bdaafbb664ec85
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-12-18 17:03:25 -0500

    Do not display queries count by default

--- db/migrate/001_update_queries.rb
+++ db/migrate/001_update_queries.rb
@@ -1,6 +1,6 @@
 class UpdateQueries < RedmineQueriesCount::LegacyMigration[4.2]
   def up
-    add_column :queries, :show_count, :boolean, :default => true
+    add_column :queries, :show_count, :boolean, :default => false
   end
 
   def down
