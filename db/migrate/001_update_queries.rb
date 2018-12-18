c67c81491442d31eaf9e8ef606e976b190453d54
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-12-18 10:25:11 -0500

    Fix style in migrations

--- db/migrate/001_update_queries.rb
+++ db/migrate/001_update_queries.rb
@@ -1,9 +1,9 @@
 class UpdateQueries < ActiveRecord::Migration
-	def up
-		add_column :queries, :show_issues_count, :boolean, :default => 0	
-	end
+  def up
+    add_column :queries, :show_count, :boolean, :default => 0
+  end
 
-	def down
-		remove_column :queries, :show_issues_count
-	end
+  def down
+    remove_column :queries, :show_count
+  end
 end
