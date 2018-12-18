7e63696fc52cad2c02a60f0087988e0ceaf15cc0
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-12-18 10:25:04 -0500

    Remove unrelated code for query count

--- db/migrate/002_add_custom_field_list_order.rb
+++
@@ -1,9 +0,0 @@
-class AddCustomFieldListOrder < ActiveRecord::Migration
-	def up
-		add_column :custom_fields, :custom_list_order, :string, :null => true, :default => nil	
-	end
-
-	def down
-		remove_column :custom_fields, :custom_list_order
-	end
-end
--- init.rb
+++ init.rb
@@ -1,24 +1,16 @@
 require 'redmine'
 
-# Patch the Redmine core
-if Rails::VERSION::MAJOR < 3
-  require 'dispatcher'
-  object_to_prepare = Dispatcher
-else
-  object_to_prepare = Rails.configuration
-end
+QUERIES_COUNT_VERSION_NUMBER = '1.1.1'
 
-object_to_prepare.to_prepare do
-  require 'advanced_queries_queries_helper_patch'
-  require 'advanced_queries_custom_field_patch'
-end
-
-# Configure our plugin
-Redmine::Plugin.register :queries_issues_count do
-  name 'Queries Issues Count'
+Redmine::Plugin.register :redmine_queries_count do
+  name 'Queries Count'
   author 'eXolnet'
-  description 'This plugin allow user to add issues count for theirs custom reports.'
-  version '1.1.1'
-  url 'https://github.com/eXolnet/redmine-queries-issues-count'
+  description 'This plugin allow user to add queries item count in the sidebar.'
+  version QUERIES_COUNT_VERSION_NUMBER
+  url 'https://github.com/eXolnet/redmine-queries-count'
   author_url 'https://www.exolnet.com'
+
+  requires_redmine :version_or_higher => '3.3'
 end
+
+require 'redmine_queries_count'
