1e4163c4e719a50f4200b247b816021cba2b27ff
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-08-07 20:32:32 -0400

    Normalize migration compatibility for Redmine 3 and 4

--- db/migrate/001_update_queries.rb
+++ db/migrate/001_update_queries.rb
@@ -1,4 +1,6 @@
-class UpdateQueries < RedmineQueriesCount::LegacyMigration[4.2]
+migration_class = ActiveRecord::VERSION::MAJOR >= 5 ? ActiveRecord::Migration[4.2] : ActiveRecord::Migration
+
+class UpdateQueries < migration_class
   def up
     add_column :queries, :show_count, :boolean, :default => false
   end
