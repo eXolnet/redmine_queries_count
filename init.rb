b66b5000201148c789c87f20eab328921409d0eb
Alexandre D’Eschambeault <adeschambeault@exolnet.com>
2023-04-25 15:43:31 -0400

    refactor: drop support for unmaintained Redmine versions

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_queries_count do
   url 'https://github.com/eXolnet/redmine_queries_count'
   author_url 'https://www.exolnet.com'
 
-  requires_redmine :version_or_higher => '3.4'
+  requires_redmine :version_or_higher => '4.2'
 end
 
 require File.dirname(__FILE__) + '/lib/redmine_queries_count'
